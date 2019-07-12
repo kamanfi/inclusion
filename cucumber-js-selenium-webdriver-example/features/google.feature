82b866508746ab1f9c2f4dc07c2f9edda24929f1
Kwasi Amanfi <kamanfi@gmail.com>
2019-07-12 14:38:35 -0400

    add on click test

--- cucumber-js-selenium-webdriver-example/features/google.feature
+++ cucumber-js-selenium-webdriver-example/features/google.feature
@@ -6,3 +6,11 @@ Feature: Searching for cucumbers
   Scenario: Google cucumber search
     When I search Google for "cucumbers"
     Then I should see some results
+
+  Scenario: Cats should return at least 8 results
+    When I search Google for "cats"
+    Then I should see at least 8 results
+
+  Scenario: I should be able to click image button and see images of cats
+    When I search Google for "cats"
+    Then I should be able to click image button and see images of cats
--- cucumber-js-selenium-webdriver-example/features/step_definitions/google-steps.js
+++ cucumber-js-selenium-webdriver-example/features/step_definitions/google-steps.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var {defineSupportCode} = require('cucumber');
-var {By, until, Key} = require('selenium-webdriver');
-var {expect} = require('chai');
+var { defineSupportCode } = require('cucumber');
+var { By, until, Key } = require('selenium-webdriver');
+var { expect } = require('chai');
 
-defineSupportCode(function({When, Then}) {
+defineSupportCode(function ({ When, Then }) {
 
   When(/^I search Google for "([^"]*)"$/, function (searchQuery, next) {
     this.driver.get('http://www.google.co.uk/webhp?complete=0');
@@ -12,18 +12,52 @@ defineSupportCode(function({When, Then}) {
       .sendKeys(searchQuery);
     this.driver.findElement(By.name('q'))
       .sendKeys(Key.ENTER)
-        .then(function() {
-          next();
-        });
+      .then(function () {
+        next();
+      });
   });
 
   Then(/^I should see some results$/, function (next) {
     this.driver.wait(until.elementLocated(By.css('div.g')));
     this.driver.findElements(By.css('div.g'))
-      .then(function(elements) {
+      .then(function (elements) {
         expect(elements.length).to.not.equal(0);
         next();
       });
   });
 
+  // When(/^I search cats for "([^"]*)"$/, function (searchQuery, next) {
+  //   this.driver.get('http://www.google.co.uk/webhp?complete=0');
+  //   this.driver.findElement(By.name('q'))
+      // .sendKeys(searchQuery);
+  //   this.driver.findElement(By.name('q'))
+  //     .sendKeys(Key.ENTER)
+  //       .then(function() {
+  //         next();
+  //       });
+  // });
+
+  Then(/^I should see at least 8 results$/, function (next) {
+    this.driver.wait(until.elementLocated(By.css('div.g')));
+    this.driver.findElements(By.css('div.g'))
+      .then(function (elements) {
+        expect(elements.length).to.not.lessThan(8);
+        next();
+      });
+  });
+
+  Then(/^I should be able to click image button and see images of cats$/, function (next) {
+    this.driver.wait(until.elementLocated(By.css('a.q.qs')));
+    this.driver.findElements(By.css('a.q.qs'))
+      .then(elements => {
+        (elements[0]).click();
+        this.driver.wait(until.elementLocated(By.css('a.rg_l')));
+        this.driver.findElements(By.css('a.rg_l'))
+          .then(elements => elements.length.to.not.lessThan(8));
+        next();
+      });
+  });
+
+
 });
+
